27c83f805bf421bc2e0b1cab7e2eb0fe47cc8ca7
Eliana c <eliana.gl.c.c@gmail.com>
2021-09-30 01:14:47 -0500

    add contract

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,4 +1,4 @@
-import { PostedMessage, messages } from './model';
+import { PostedMessage, messages, PostedEvent, events } from './model';
 
 // --- contract code goes below
 
@@ -21,12 +21,74 @@ export function addMessage(text: string): void {
  * Returns an array of last N messages.\
  * NOTE: This is a view method. Which means it should NOT modify the state.
  */
-export function getMessages(): PostedMessage[] {
+const curr =  "tammm.testnet"
+export function getMessages(accountId: string): PostedMessage[] { // currentUser: string
   const numMessages = min(MESSAGE_LIMIT, messages.length);
   const startIndex = messages.length - numMessages;
   const result = new Array<PostedMessage>(numMessages);
   for(let i = 0; i < numMessages; i++) {
     result[i] = messages[i + startIndex];
+  }
+  // let curr =  "tammm.testnet"
+  // curr = accountId;
+ 
+  const resultf = result.filter(message => message.sender == curr);
+  return resultf;
+}
+
+/**
+ * Returns an array of last N messages.\
+ * NOTE: This is a view method. Which means it should NOT modify the state.
+ */
+ export function getAllMessages(): PostedMessage[] {
+  const numMessages = min(MESSAGE_LIMIT, messages.length);
+  const startIndex = messages.length - numMessages;
+  const result = new Array<PostedMessage>(numMessages);
+  for(let i = 0; i < numMessages; i++) {
+    result[i] = messages[i + startIndex];
+  }
+  return result;
+}
+
+/**
+ * Returns an array of last N messages.\
+ * NOTE: This is a view method. Which means it should NOT modify the state.
+ */
+//  export function getMessages(): PostedMessage[] {
+//   const numMessages = min(MESSAGE_LIMIT, messages.length);
+//   const startIndex = messages.length - numMessages;
+//   const result = new Array<PostedMessage>(numMessages);
+//   for(let i = 0; i < numMessages; i++) {
+//     result[i] = messages[i + startIndex];
+//   }
+//   return result;
+// }
+
+
+/// EVENTS
+
+/**
+ * Adds a new message under the name of the sender's account id.\
+ * NOTE: This is a change method. Which means it will modify the state.\
+ * But right now we don't distinguish them with annotations yet.
+ */
+ export function addEvent(text: string, code: string, dateStart: string, dateEnd: string): void {
+  // Creating a new message and populating fields with our data
+  const event = new PostedEvent(text, code, dateStart, dateEnd);
+  // Adding the message to end of the the persistent collection
+  events.push(event);
+}
+
+/**
+ * Returns an array of last N messages.\
+ * NOTE: This is a view method. Which means it should NOT modify the state.
+ */
+ export function getAllEvents(): PostedEvent[] {
+  const numEvents = min(MESSAGE_LIMIT, events.length);
+  const startIndex = events.length - numEvents;
+  const result = new Array<PostedEvent>(numEvents);
+  for(let i = 0; i < numEvents; i++) {
+    result[i] = events[i + startIndex];
   }
   return result;
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -19,3 +19,24 @@ export class PostedMessage {
  * It will be used as a prefix to all keys required to store data in the storage.
  */
 export const messages = new PersistentVector<PostedMessage>("m");
+
+/**  */
+/** 
+ * Exporting a new class PostedEvent so it can be used outside of this file.
+ */
+@nearBindgen
+export class PostedEvent {
+  premium: boolean;
+  sender: string;
+  constructor(public text: string, public codeEvent: string, public dateStart: string, public dateEnd: string) {
+    this.premium = context.attachedDeposit >= u128.from('10000000000000000000000');
+    this.sender = context.sender;
+  }
+}
+/**
+* collections.vector is a persistent collection. Any changes to it will
+* be automatically saved in the storage.
+* The parameter to the constructor needs to be unique across a single contract.
+* It will be used as a prefix to all keys required to store data in the storage.
+*/
+export const events = new PersistentVector<PostedEvent>("n");
