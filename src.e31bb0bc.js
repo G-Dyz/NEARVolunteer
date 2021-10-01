3765614cebe30e9dd716c675fea2b280a5c84e3d
Eliana c <eliana.gl.c.c@gmail.com>
2021-10-01 15:26:42 -0500

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -49053,7 +49053,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60916" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51780" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
